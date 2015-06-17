ec897923ec08e01e54a0fe929cf590b76f242fe0
Mikul Gohil <mikulgohil@gmail.com>
2015-06-17 09:39:45 +0530

    App js added

---
+++ imdb/scripts/app.js
@@ -0,0 +1,19 @@
+﻿var app = angular.module("imdbApp", []);
+
+app.config(function($httpProvider) {
+    //delete $httpProvider.defaults.headers.common['X-Requested-With'];
+});
+
+////app.filter('unique', function () {
+////    return function (input, key) {
+////        var unique = {};
+////        var uniqueList = [];
+////        for (var i = 0; i < input.length; i++) {
+////            if (typeof unique[input[i][key]] == "undefined") {
+////                unique[input[i][key]] = "";
+////                uniqueList.push(input[i]);
+////            }
+////        }
+////        return uniqueList;
+////    };
+////});
